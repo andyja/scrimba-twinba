e456443bc71b0b592c884dac72fe8a9bae61fd7c
Andy Jackson <andypauljackson@gmail.com>
2023-05-02 15:27:29 +0100

    accesed value of textarea and logged to console

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import { tweetsData } from './data.js';
+const tweetInput = document.getElementById("tweet-input");
+const tweetBtn = document.getElementById("tweet-btn");
 
-console.log(tweetsData);
+
+tweetBtn.addEventListener('click', function(){
+    console.log(tweetInput.value);
+})
+
