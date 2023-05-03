4491f1c75e17d94ccd801a60c9aa69aac4c8ce6f
Andy Jackson <andypauljackson@gmail.com>
2023-05-03 15:53:48 +0100

    like tweet functionality added

--- index.js
+++ index.js
@@ -7,8 +7,26 @@ tweetBtn.addEventListener('click', function(){
     console.log(tweetInput.value);
 })
 
+document.addEventListener('click', function(e){
+    if(e.target.dataset.like){
+        handleLikeClick(e.target.dataset.like)
+    }
+})
+
+function handleLikeClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+        
+    })[0]
+    targetTweetObj.likes++
+    render();
+    console.log(targetTweetObj) 
+}
+
+
 function getFeedHtml(){
     let feedHtml = ``;
+
     tweetsData.forEach(function(tweet){
         feedHtml += `
 <div class="tweet">
@@ -19,12 +37,20 @@ function getFeedHtml(){
             <p class="tweet-text">"${tweet.tweetText}"</p>
             <div class="tweet-details">
                 <span class="tweet-detail">
+                        <i class="fa-regular fa-comment-dots"
+                        data-reply="${tweet.uuid}"
+                        ></i>
                         ${tweet.replies.length}
                     </span>
                     <span class="tweet-detail">
+                        <i class="fa-solid fa-heart"
+                        data-like="${tweet.uuid}"
+                        ></i>
                         ${tweet.likes}
                     </span>
                     <span class="tweet-detail">
+                        <i class="fa-solid fa-retweet"
+                        data-retweet="${tweet.uuid}"></i>
                         ${tweet.retweets}
                     </span>
                 </div>   
@@ -34,4 +60,8 @@ function getFeedHtml(){
     })
 return feedHtml;
 }
-console.log(getFeedHtml());
+function render(){
+    document.getElementById("feed").innerHTML = getFeedHtml(); 
+}
+
+render();
